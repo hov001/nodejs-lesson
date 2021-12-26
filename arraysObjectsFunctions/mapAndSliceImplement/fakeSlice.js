b75672d1a7505ca92c06f0b46a85ec7fc1bd18eb
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-12-26 19:44:15 +0400

    fix(fakeSlice.js): fix bug in the fakeSlice.js

--- arraysObjectsFunctions/mapAndSliceImplement/fakeSlice.js
+++ arraysObjectsFunctions/mapAndSliceImplement/fakeSlice.js
@@ -4,7 +4,7 @@ Array.prototype.fakeSlice = function (begin = 0, end) {
     beg = Number(begin) || 0;
 
   let size,
-    start = beg >= 0 ? beg : length + beg,
+    start = beg > 0 ? beg : length + beg,
     sliceTo = end ? end : length;
 
   if (end < 0) {
@@ -20,5 +20,5 @@ Array.prototype.fakeSlice = function (begin = 0, end) {
   return newArr;
 };
 
-console.log([1, 2, 3].fakeSlice(-1));
-console.log([1, 2, 3].slice(-1));
+console.log([1, 2, 3].fakeSlice(0, 0));
+console.log([1, 2, 3].slice(0, 0));
